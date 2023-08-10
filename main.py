17e3bd98a7c8b500ec65374d71e2d721e0a52f7b
angelopol <angelopolgrossi18@gmail.com>
2023-08-10 15:10:58 -0400

    Add main

---
+++ main.py
@@ -0,0 +1,15 @@
+list = []
+
+while True:
+
+    list.append(input("type a word "))
+
+    while len(list) > 3:
+
+        try:
+
+            print(list.index(input("search a entered word ")))
+
+        except:
+
+            print("the word is not registered in the list")
